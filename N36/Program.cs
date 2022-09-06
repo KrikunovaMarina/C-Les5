959afff5e4a7118c2417e83ef616af8a505e9f37
Marina <umarishka@bk.ru>
2022-09-06 20:25:37 +0300

    add N34 N36 N38

---
+++ N36/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами.
+ Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+ */
+ int size = 4;
+int [] array = new int [size];
+for (int i =0; i<array.Length; ++i )
+
+{
+    array[i]= new Random().Next(-100,100);
+}
+Console.WriteLine(string.Join( ",", array)); 
+int a= array[0]+array[2];
+Console.WriteLine("Сумма элементов стоящих на нечетных позициях ->" + a);
---
+++ N38/Program.cs
@@ -0,0 +1,21 @@
+﻿int size = 5;
+double [] array = new double [size];
+for (int i =0; i<array.Length; ++i )
+{
+    array[i]= new Random().NextDouble();
+}
+Console.WriteLine(string.Join( ",", array)); 
+
+double max = array[0];
+if (array[1]> max) max = array[1];
+if (array[2]> max) max = array[2];
+if (array[3]> max) max = array[3];
+if (array[4]> max) max = array[4];
+double min = array[0];
+if (array[1]< min) min = array[1];
+if (array[2]< min) min = array[2];
+if (array[3]< min) min = array[3];
+if (array[4]< min) min = array[4];
+double raz = 0;
+raz = max - min;
+Console.WriteLine("max = " + max + " min = " + min + " max - min = " + raz);
